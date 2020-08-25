f5fd28f9e54533ba4823c73d1fae6c71a96f77a0
Quinton Tran <quintonqt@gmail.com>
2020-08-25 16:27:50 -0400

    Added Bootstrap and made initial scoreboard structure

--- src/App.js
+++ src/App.js
@@ -1,50 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return (
-      <div id="wrapper">HI</div>
-        <MatchFinder />
-        <Ranker />
-        <Scoreboard />
-    )
-  }
+	render() {
+		return (
+			<Container>
+				<MatchFinder />
+
+				<Row>
+					<Col md="6">
+						<Ranker />
+					</Col>
+					<Col md="6">
+						<Scoreboard />
+					</Col>
+				</Row>
+			</Container>
+		);
+	}
 }
 
 class MatchFinder extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return
-  }
+	render() {
+		return <div></div>;
+	}
 }
 
 class Ranker extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return
-  }
+	render() {
+		return <div></div>;
+	}
 }
 
 class Scoreboard extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return
-  }
+	render() {
+		return (
+			<div id="scorecard">
+				<h1>Score</h1>
+				<p id="sport-name">Volleyball</p>
+				<div id="current-score">
+					<p id="team1-score" class="score">
+						25
+					</p>
+					<p>-</p>
+					<p id="team2-score" class="score">
+						24
+					</p>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
