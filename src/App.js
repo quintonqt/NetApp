acf1ea929309b43343e1b13f991cadc89496cd65
Quinton Tran <quintonqt@gmail.com>
2020-08-26 17:04:50 -0400

    Added redux to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Container, Row, Col } from 'reactstrap';
+import { createStore } from 'redux';
 
 class App extends React.Component {
 	constructor(props) {
@@ -55,15 +56,39 @@ class Scoreboard extends React.Component {
 			<div id="scorecard">
 				<h1>Score</h1>
 				<p id="sport-name">Volleyball</p>
-				<div id="current-score">
-					<p id="team1-score" class="score">
-						25
-					</p>
-					<p>-</p>
-					<p id="team2-score" class="score">
-						24
-					</p>
-				</div>
+				<Row>
+					<Col md="4">
+						<p id="team1-score" className="score">
+							25
+						</p>
+						<p id="team1-name" className="teamName">
+							Team 1
+						</p>
+						<button type="button" class="btn btn-success">
+							+
+						</button>
+						<button type="button" class="btn btn-danger">
+							-
+						</button>
+					</Col>
+					<Col md="4">
+						<p>-</p>
+					</Col>
+					<Col md="4">
+						<p id="team2-score" className="score">
+							24
+						</p>
+						<p id="team2-name" className="teamName">
+							Team 2
+						</p>
+						<button type="button" class="btn btn-success">
+							+
+						</button>
+						<button type="button" class="btn btn-danger">
+							-
+						</button>
+					</Col>
+				</Row>
 			</div>
 		);
 	}
